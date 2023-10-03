bd7f6d1aa02525a9fab6f36c32fecc96c6f76776
zhaihongguang <33441387+zhaihongguang@users.noreply.github.com>
2023-10-03 15:56:24 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -7,12 +7,13 @@ apt -y install nano
 apt -y install htop
 apt -y install nvtop
 apt -y install cmake
+apt -y install python3-pip
 
 # Clone the repository and build the project
 git clone https://github.com/shanhaicoder/XENGPUMiner.git
 cd XENGPUMiner
 chmod +x build.sh
-./build.sh
+./build.sh -cuda_arch sm_86
 
 # Update the configuration file
 sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xbC95Eb768Fa40d6281e113F8da21095a9e535124/g' config.conf
